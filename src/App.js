a8a74e8f5a1a0cfe37a8fd14b764f22aaab008ba
rick427 <44168932+rick427@users.noreply.github.com>
2019-06-04 15:10:36 +0100

    refactoring class compt to functional compt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Navbar/>
+                <Navbar title="Github User Finder" icon="fab fa-github" />
+                <div className="container">
+                  <Users/>
+                </div>
             </div>
         )
     }
